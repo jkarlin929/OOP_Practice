c0538f64b7a6160e440d4cf9dff23d2a2f35ca32
jkarlin929 <osida11572@gmail.com>
2018-12-17 23:07:16 -0500

    add square.rb

--- src/square.rb
+++ src/square.rb
@@ -5,4 +5,8 @@ class Square < Rectangle
   # it should only take one argument
   # and call super with the correct number of arguments
   # how many arguments does the super class take?
+
+  def initialize(side)
+    super(side, side)
+  end
 end
